69ce106005caef53260af3cdf228b53f3bdb672b
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-08-20 15:36:58 +0200

    Add title and mui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Typography } from '@mui/material';
 import './App.css';
 import ChartComponent from './Components/Chart';
 
 function App() {
   return (
     <div className="App">
+      <Typography>FinanSense</Typography>
       <ChartComponent />
     </div>
   );
